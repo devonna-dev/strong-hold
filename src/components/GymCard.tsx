3a168fe40c3b1d1b8c6c9efa030cbb16b091b28e
Dev Leslie <devonna.dev@gmail.com>
2025-06-04 22:00:47 -0400

    First style pass on gym card

--- src/components/GymCard.tsx
+++ src/components/GymCard.tsx
@@ -1,14 +1,26 @@
+import styled from "styled-components";
 import type GymType from "../types/GymType";
 
 
 const GymCard = ({ gymDatum }: { gymDatum: GymType }) => {
     return (
-        <div id={gymDatum.name}>
+        <StyledGymCardDiv id={gymDatum.name}>
             {gymDatum.name}
-        </div>
+        </StyledGymCardDiv>
     )
 }
 
+const StyledGymCardDiv = styled.div`
+    margin: auto;
+    margin-top: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 250px;
+    width: 50%;
+    box-shadow: 0px 7px 14px 0px hsl(0deg 0% 0% / 0.47);
+    border-radius: 25px;
 
-
+`
+// box - shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
 export default GymCard;

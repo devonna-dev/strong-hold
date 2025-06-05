bb3f2c8810725ab36c581ca49d679e0b4b6d0cea
Dev Leslie <devonna.dev@gmail.com>
2025-06-04 22:12:35 -0400

    Begin creating GymCard.
    Still needs polishing

--- src/components/GymCard.tsx
+++ src/components/GymCard.tsx
@@ -5,22 +5,43 @@ import type GymType from "../types/GymType";
 const GymCard = ({ gymDatum }: { gymDatum: GymType }) => {
     return (
         <StyledGymCardDiv id={gymDatum.name}>
-            {gymDatum.name}
+            <header>
+                <h2>{gymDatum.name}</h2>
+            </header>
+            <div className={'content'}>
+                <p>
+                    {gymDatum.description}
+                </p>
+                <p>
+                    {gymDatum.address.lineOne}
+                </p>
+                <p>
+                    {gymDatum.address.lineTwo && gymDatum.address.lineTwo}
+                </p>
+                <p>
+                    {gymDatum.address.city}
+                </p>
+                <p>
+                    {gymDatum.address.state}
+                </p>
+                <p>
+                    {gymDatum.address.zip}
+                </p>
+            </div>
         </StyledGymCardDiv>
     )
 }
 
 const StyledGymCardDiv = styled.div`
-    margin: auto;
-    margin-top: 25px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 250px;
+    margin: 0 auto;
+    margin-top: 10px;
+    height: 300px;
     width: 50%;
     box-shadow: 0px 7px 14px 0px hsl(0deg 0% 0% / 0.47);
     border-radius: 25px;
-
+    
+    display: flex;
+    flex-direction: column;
 `
 // box - shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
 export default GymCard;
